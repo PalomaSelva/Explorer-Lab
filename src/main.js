eb28b17c33e879e30ac29536fd13fd859d8789cd
PalomaSelva <palomaselva1@gmail.com>
2022-11-15 20:49:45 -0300

    commit teste

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import './css/style.css';
 import IMask from 'imask';
+
+const bgShadow1 = window.document.querySelector('.cc svg > g g:nth-child(1) path')
 function setCardType(type){
     const colors ={
         visa:["#2D57F2","#436D99"],
         mastercard:["#DF6F29","#C69347"],
         default:["black","gray"],
     }
-    
-    const img = new URL('./public/${type}.svg', import.meta.url)
+
 
     bgShadow1.setAttribute('fill',colors[type][0])
     bgShadow2.setAttribute('fill',colors[type][1])
-    ccIcon.setAttribute("src", img)
+    ccIcon.setAttribute("src", `./public/${type}.svg`)
 }
-const bgShadow1 = window.document.querySelector('.cc svg > g g:nth-child(1) path')
 const bgShadow2 = window.document.querySelector('.cc svg > g g:nth-child(2) path')
 const ccIcon = window.document.querySelector('.cc-logo span:nth-child(2) img:nth-child(1)')
 
